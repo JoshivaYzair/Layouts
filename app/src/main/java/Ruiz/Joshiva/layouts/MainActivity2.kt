347631e381829df092673d23775b036cb353a37e
Joshiva Yzair <joshivatapia@gmail.com>
2024-02-19 18:28:59 -0600

    Layout

---
+++ app/src/main/java/Ruiz/Joshiva/layouts/MainActivity2.kt
@@ -0,0 +1,11 @@
+package Ruiz.Joshiva.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
